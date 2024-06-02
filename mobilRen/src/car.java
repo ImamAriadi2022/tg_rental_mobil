36b9c4176800e0145e6671f20647fa9a95bb136c
Imam ariadi <gamerapin139@gmail.com>
2024-06-02 22:38:43 +0700

    update object car

---
+++ mobilRen/src/car.java
@@ -0,0 +1,71 @@
+public class car {
+    private int ID;
+    private String brand;
+    private String model;
+    private String color;
+    private int year;
+    private double price;
+    private boolean available;
+
+
+    public car() {}
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID() {
+        this.ID = ID;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand() {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(){
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(){
+        this.year = year;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice() {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailanle() {
+        this.available = available;
+    }
+
+
+
+}
